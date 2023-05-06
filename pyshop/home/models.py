a1150757ecc642406c4f0d1135fe37912840f250
Volodymyr Shargorodskyi <gsmkibernetik@gmail.com>
2023-05-06 14:51:32 +0300

    create models

--- pyshop/home/models.py
+++ pyshop/home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    image = models.ImageField(max_length=100, blank=True)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    image = models.ImageField(max_length=100, blank=True)
+    count = models.IntegerField(default=0, blank=True)
+    availability = models.BooleanField(default=True, blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField()
