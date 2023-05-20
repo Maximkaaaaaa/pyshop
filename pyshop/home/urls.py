3dbf4c893fbde2639a989b52c588149777fd7b8e
TheBreedY <stekolshchikov.yurii@gmail.com>
2023-05-20 14:54:59 +0300

    created media holder

--- pyshop/home/urls.py
+++ pyshop/home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import catalog, IndexView
+from .views import catalog, IndexView, CategoryCreateView
 
 urlpatterns = [
     # path('', index, name='index'),
     path('', IndexView.as_view(), name='index'),
     path('catalog/', catalog, name='catalog'),
+    path('category/create/', CategoryCreateView.as_view(), name='categories_create')
 ]
